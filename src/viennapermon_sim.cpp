a42132d9f5985ff619a0187f5796ee98f722cada
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2015-08-27 12:59:48 +0200

    better logging, adapted to viennagrid changes

--- src/viennapermon_sim.cpp
+++ src/viennapermon_sim.cpp
@@ -17,20 +17,29 @@
 
 void process_local(viennamesh::context_handle & context,
                    int mpi_rank,
-                   viennagrid::mesh_t mesh_local)
+                   viennagrid::mesh mesh_local)
 {
   viennamesh::algorithm_handle mark_hull_regions = context.make_algorithm("mark_hull_regions");
   mark_hull_regions.set_input( "mesh", mesh_local.internal() );
-  mark_hull_regions.run();
+  {
+    viennamesh::LoggingStack s("mark_hull_regions");
+    mark_hull_regions.run();
+  }
 
   viennamesh::algorithm_handle mesher = context.make_algorithm("netgen_make_mesh");
   mesher.set_default_source(mark_hull_regions);
-  mesher.run();
+  {
+    viennamesh::LoggingStack s("netgen_make_mesh");
+    mesher.run();
+  }
 
   viennamesh::algorithm_handle mesh_writer = context.make_algorithm("mesh_writer");
   mesh_writer.set_default_source(mesher);
   mesh_writer.set_input( "filename", "mpi_mesh_" + boost::lexical_cast<std::string>(mpi_rank) + "_mesh.vtu" );
-  mesh_writer.run();
+  {
+    viennamesh::LoggingStack s("mesh_writer");
+    mesh_writer.run();
+  }
 }
 
 int main(int argc, char* argv[])
@@ -40,7 +49,7 @@ int main(int argc, char* argv[])
   MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
-  typedef viennagrid::mesh_hierarchy_t MeshHierarchyType;
+  typedef viennagrid::mesh_hierarchy MeshHierarchyType;
   typedef viennagrid::result_of::mesh<MeshHierarchyType>::type MeshType;
 
   if(argc != 2)
@@ -68,16 +77,26 @@ int main(int argc, char* argv[])
 
     viennamesh::algorithm_handle mesh_reader = context.make_algorithm("plc_reader");
     mesh_reader.set_input( "filename", filename );
-    mesh_reader.run();
+    {
+      viennamesh::LoggingStack s("plc_reader");
+      mesh_reader.run();
+    }
 
     viennamesh::algorithm_handle mesher = context.make_algorithm("tetgen_make_mesh");
     mesher.set_default_source(mesh_reader);
-    mesher.run();
+    mesher.set_input("cell_size", 10.0);
+    {
+      viennamesh::LoggingStack s("tetgen_make_mesh");
+      mesher.run();
+    }
 
     viennamesh::algorithm_handle metis_partitioning = context.make_algorithm("metis_mesh_partitioning");
     metis_partitioning.set_default_source(mesher);
     metis_partitioning.set_input( "region_count", mpi_size );
-    metis_partitioning.run();
+    {
+      viennamesh::LoggingStack s("metis_mesh_partitioning");
+      metis_partitioning.run();
+    }
 
     viennamesh::algorithm_handle extract_boundary = context.make_algorithm("extract_boundary");
     extract_boundary.set_default_source(metis_partitioning);
@@ -87,39 +106,52 @@ int main(int argc, char* argv[])
     extract_plc_geometry.set_default_source(extract_boundary);
     extract_plc_geometry.set_input("coplanar_tolerance", 1e-2);
     extract_plc_geometry.set_input("colinear_tolerance", 1e-2);
-    extract_plc_geometry.run();
+    {
+      viennamesh::LoggingStack s("extract_plc_geometry");
+      extract_plc_geometry.run();
+    }
 
     viennamesh::algorithm_handle triangle_make_hull = context.make_algorithm("triangle_make_hull");
     triangle_make_hull.set_default_source(extract_plc_geometry);
     triangle_make_hull.set_input("cell_size", 1.0);
-    triangle_make_hull.run();
+    {
+      viennamesh::LoggingStack s("triangle_make_hull");
+      triangle_make_hull.run();
+    }
 
     viennamesh::algorithm_handle mark_hull_regions = context.make_algorithm("mark_hull_regions");
     mark_hull_regions.set_default_source(triangle_make_hull);
-    mark_hull_regions.run();
+    {
+      viennamesh::LoggingStack s("mark_hull_regions");
+      mark_hull_regions.run();
+    }
 
     viennamesh::algorithm_handle split_mesh = context.make_algorithm("split_mesh");
     split_mesh.set_default_source(mark_hull_regions);
-    split_mesh.run();
-
+    {
+      viennamesh::LoggingStack s("split_mesh");
+      split_mesh.run();
+    }
 
     // Distribute the partitions
     // partition-0 is processed by the master process (this process, mpi_rank=0)
     // partitions-1..n are processed by the worker processes (mpi_rank=1..mpi_size-1)
     //
+
+//     viennamesh::data_handle<viennagrid_mesh> meshes = split_mesh.get_output<viennagrid_mesh>( "mesh" );
     for(int target = 1; target < mpi_size; target++)
     {
-      viennamesh::data_handle<viennagrid_mesh> mesh = split_mesh.get_output<viennagrid_mesh>( "mesh[" + boost::lexical_cast<std::string>(target) + "]" );
-
-      MeshHierarchyType mh = mesh().mesh_hierarchy();
+      viennagrid::mesh mesh = split_mesh.get_output<viennagrid_mesh>("mesh[" + boost::lexical_cast<std::string>(target) + "]")();
+      MeshHierarchyType mh = mesh.get_mesh_hierarchy();
       viennagrid::mpi::send(mh, target, MPI_COMM_WORLD);
     }
 
     // Master simulates the first partition
     //
 
-    viennamesh::data_handle<viennagrid_mesh> mesh = split_mesh.get_output<viennagrid_mesh>( "mesh[0]" );
-    process_local(context, mpi_rank, mesh());
+//     viennagrid::mesh mesh = meshes(0);
+    viennagrid::mesh mesh = split_mesh.get_output<viennagrid_mesh>("mesh[0]")();
+    process_local(context, mpi_rank, mesh);
   }
   // Worker processes
   else
