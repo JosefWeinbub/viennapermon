82fee1a14cac0165d0e886bd93b6d2f1de78d1ac
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2015-09-02 16:11:34 +0200

    added 'viennagrid2libmesh' conversion test
    and required auxiliary CMake tools for interfacing with libMesh
    also updated the Readme.md

--- README.md
+++ README.md
@@ -3,3 +3,31 @@
 This is the development repository for the joint effort of interfacing the Vienna* libraries with Permon* libraries.
 The goal is to build a large-scale distributed FETI application by making use of synergy effects provided by 
 different free open source scientific software infrastructures.
+
+Dependencies:
+- CMake
+- Metis
+- MPI
+- LibMesh
+- ViennaUtils: https://github.com/viennautils/viennautils-dev  branch: master
+- ViennaGrid:  https://github.com/viennagrid/viennagrid-dev    branch: FlorianRudolf/feature-viennagrid-3
+- ViennaMesh:  https://github.com/viennamesh/viennamesh-dev    branch: FlorianRudolf/dev-viennamesh-2
+
+
+Environment variables:
+export VIENNAUTILSPATH=/path/to/your/viennautils-dev
+export VIENNAGRIDPATH=/path/to/your/viennagrid-dev
+export VIENNAMESHPATH=/path/to/your/viennamesh-dev 
+
+
+Configure:
+cmake -DLIBMESH_DIR=/path/to/libmesh/instal/directory/ ..
+
+
+
+
+
+libMesh Build Instructions: (tested with 0.9.4)
+  ./configure --prefix=/install/path/ --disable-mpi
+
+
---
+++ tests/viennagrid2libmesh.cpp
@@ -0,0 +1,84 @@
+
+// System includes
+#include <iostream>
+#include <mpi.h>
+
+// ViennaGrid includes
+#include "viennagridpp/core.hpp"
+#include "viennagridpp/io/netgen_reader.hpp"
+
+// libMesh includes
+#include "libmesh/mesh.h"
+#include "libmesh/serial_mesh.h"
+#include "libmesh/point.h"
+#include "libmesh/node.h"
+#include "libmesh/elem.h"
+#include "libmesh/cell_tet4.h"
+#include "libmesh/gmsh_io.h"
+
+int main(int argc, char* argv[])
+{
+  //  ----
+  // Setup test ViennaGrid mesh
+  //  ----
+  typedef viennagrid::mesh                                          MeshType;
+  typedef viennagrid::result_of::vertex_range<MeshType>::type       VertexRange;
+  typedef viennagrid::result_of::iterator<VertexRange>::type        VertexIterator;
+  typedef viennagrid::result_of::cell_range<MeshType>::type       CellRange;
+  typedef viennagrid::result_of::iterator<CellRange>::type          CellIterator;
+  typedef viennagrid::result_of::element<MeshType>::type            CellType;
+  typedef viennagrid::result_of::vertex_range<CellType>::type       VertexOnCellRange;
+  typedef viennagrid::result_of::iterator<VertexOnCellRange>::type  VertexOnCellIterator;
+
+  MeshType   vgrid_mesh;
+  viennagrid::io::netgen_reader reader;
+  reader(vgrid_mesh, "../../tests/data/cube48.mesh");
+
+  //  ----
+  // Transfer to libMesh mesh data structure
+  //  ----
+  libMesh::LibMeshInit init (argc, argv);
+  libMesh::SerialMesh libmesh(init.comm());
+  libmesh.set_mesh_dimension(3);
+
+  viennagrid::mesh_point_accessor mesh_point_accessor(vgrid_mesh);
+
+  unsigned int i = 0;
+  std::map<unsigned int, unsigned int> nodetrans;
+
+  VertexRange vgrid_vertices(vgrid_mesh);
+  for (VertexIterator vit = vgrid_vertices.begin();  //STL-like iteration
+                      vit != vgrid_vertices.end();
+                    ++vit)
+  {
+    libmesh.add_point(libMesh::Point(mesh_point_accessor.get( *vit )[0], mesh_point_accessor.get( *vit )[1], mesh_point_accessor.get( *vit )[2]), i);
+    nodetrans[(*vit).id()] = i++;
+  }
+
+  CellRange cells_mesh(vgrid_mesh);
+  for (CellIterator cit = cells_mesh.begin();
+                    cit != cells_mesh.end();
+                  ++cit)
+  {
+    libMesh::Elem* elem = new libMesh::Tet4();
+
+    VertexOnCellRange vertex_on_cells(*cit);
+    int j = 0;
+    for(VertexOnCellIterator vocit  = vertex_on_cells.begin();
+                             vocit != vertex_on_cells.end();
+                           ++vocit)
+    { 
+      elem->set_node(j) = libmesh.node_ptr(libMesh::cast_int<libMesh::dof_id_type>( nodetrans[ (*vocit).id() ] ));
+
+      j++;
+    }
+    elem->set_id((*cit).id());
+    libmesh.add_elem(elem);
+ }
+
+  libMesh::GmshIO io(libmesh);
+  io.write("libmesh_output.msh");
+
+
+
+}
